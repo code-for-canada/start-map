2b4275684167882a6ae0e1543fedf095ae9af867
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-02-14 11:22:09 -0500

    Re-indent to create-react-app standards.

--- src/slides.js
+++ src/slides.js
@@ -18,19 +18,19 @@ export default class DynamicSlides extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       adaptiveHeight: true
-      
+
     };
     return (
       <div>
-        <Slider {...settings}>
-          {this.props.slides.map(function(slide) {
-            return (
-              <div key={slide.key}>
-                <img src={slide.img} onError={(e)=>{e.target.onerror = null; e.target.src="/imgs/undefined.jpg"}}/>
-              </div>
-            );
-          })}
-        </Slider>
+      <Slider {...settings}>
+      {this.props.slides.map(function(slide) {
+        return (
+          <div key={slide.key}>
+          <img src={slide.img} onError={(e)=>{e.target.onerror = null; e.target.src="/imgs/undefined.jpg"}}/>
+          </div>
+        );
+      })}
+      </Slider>
       </div>
     );
   }
