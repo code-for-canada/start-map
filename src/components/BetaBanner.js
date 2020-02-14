3e9836be8cf6a55682ded9cfb74124e693029aff
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-02-14 13:58:49 -0500

    Fix create-react-app linting concern with BetaBanner constructor.

--- src/components/BetaBanner.js
+++ src/components/BetaBanner.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 class BetaBanner extends Component {
-  constructor(props){
-    super(props);
-  }
   render () {
     return(
       <div className="banner-wrapper">
