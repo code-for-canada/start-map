53d5d563057366eabf1307693a4fe61169c28204
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-02-14 11:22:09 -0500

    Re-indent to create-react-app standards.

--- src/constants.js
+++ src/constants.js
@@ -1,205 +1,205 @@
 export const YEAR_OPTS = [
-    {
-        value: '2012',
-        label: '2012'
-    },
-    {
-        value: '2013',
-        label: '2013'
-    },
-    {
-        value: '2014',
-        label: '2014'
-    },
-    {
-        value: '2015',
-        label: '2015'
-    },
-    {
-        value: '2016',
-        label: '2016'
-    },
-    {
-        value: '2017',
-        label: '2017'
-    },
-    {
-        value: '2018',
-        label: '2018'
-    }
+  {
+    value: '2012',
+    label: '2012'
+  },
+  {
+    value: '2013',
+    label: '2013'
+  },
+  {
+    value: '2014',
+    label: '2014'
+  },
+  {
+    value: '2015',
+    label: '2015'
+  },
+  {
+    value: '2016',
+    label: '2016'
+  },
+  {
+    value: '2017',
+    label: '2017'
+  },
+  {
+    value: '2018',
+    label: '2018'
+  }
 ];
 
 export const WARD_OPTS = [
-    {
-        value: '1',
-        label: '1 – Etobicoke North'
-    },
-    {
-        value: '2',
-        label: '2 – Etobicoke Centre'
-    },
-    {
-        value: '3',
-        label: '3 – Etobicoke-Lakeshore'
-    },
-    {
-        value: '4',
-        label: '4 – Parkdale-High Park'
-    },
-    {
-        value: '5',
-        label: '5 – York-South Weston'
-    },
-    {
-        value: '6',
-        label: '6 – York Centre'
-    },
-    {
-        value: '7',
-        label: '7 – Humber River-Black Creek'
-    },
-    {
-        value: '8',
-        label: '8 – Eglinton-Lawrence'
-    },
-    {
-        value: '9',
-        label: '9 – Davenport'
-    },
-    {
-        value: '10',
-        label: '10 – Spadina-Fort York'
-    },
-    {
-        value: '11',
-        label: '11 – University-Rosedale'
-    },
-    {
-        value: '12',
-        label: '12 – Toronto-St. Paul’s'
-    },
-    {
-        value: '13',
-        label: '13 – Toronto Centre'
-    },
-    {
-        value: '14',
-        label: '14 – Toronto-Danforth'
-    },
-    {
-        value: '15',
-        label: '15 – Don Valley West'
-    },
-    {
-        value: '16',
-        label: '16 – Don Valley East'
-    },
-    {
-        value: '17',
-        label: '17 – Don Valley North'
-    },
-    {
-        value: '18',
-        label: '18 – Willowdale'
-    },
-    {
-        value: '19',
-        label: '19 – Beaches-East York'
-    },
-    {
-        value: '20',
-        label: '20 – Scarborough Southwest'
-    },
-    {
-        value: '21',
-        label: '21 – Scarborough Centre'
-    },
-    {
-        value: '22',
-        label: '22 – Scarborough-Agincourt'
-    },
-    {
-        value: '23',
-        label: '23 – Scarborough North'
-    },
-    {
-        value: '24',
-        label: '24 – Scarborough-Guildwood'
-    },
-    {
-        value: '25',
-        label: '25 – Scarborough-Rouge Park'
-    }
+  {
+    value: '1',
+    label: '1 – Etobicoke North'
+  },
+  {
+    value: '2',
+    label: '2 – Etobicoke Centre'
+  },
+  {
+    value: '3',
+    label: '3 – Etobicoke-Lakeshore'
+  },
+  {
+    value: '4',
+    label: '4 – Parkdale-High Park'
+  },
+  {
+    value: '5',
+    label: '5 – York-South Weston'
+  },
+  {
+    value: '6',
+    label: '6 – York Centre'
+  },
+  {
+    value: '7',
+    label: '7 – Humber River-Black Creek'
+  },
+  {
+    value: '8',
+    label: '8 – Eglinton-Lawrence'
+  },
+  {
+    value: '9',
+    label: '9 – Davenport'
+  },
+  {
+    value: '10',
+    label: '10 – Spadina-Fort York'
+  },
+  {
+    value: '11',
+    label: '11 – University-Rosedale'
+  },
+  {
+    value: '12',
+    label: '12 – Toronto-St. Paul’s'
+  },
+  {
+    value: '13',
+    label: '13 – Toronto Centre'
+  },
+  {
+    value: '14',
+    label: '14 – Toronto-Danforth'
+  },
+  {
+    value: '15',
+    label: '15 – Don Valley West'
+  },
+  {
+    value: '16',
+    label: '16 – Don Valley East'
+  },
+  {
+    value: '17',
+    label: '17 – Don Valley North'
+  },
+  {
+    value: '18',
+    label: '18 – Willowdale'
+  },
+  {
+    value: '19',
+    label: '19 – Beaches-East York'
+  },
+  {
+    value: '20',
+    label: '20 – Scarborough Southwest'
+  },
+  {
+    value: '21',
+    label: '21 – Scarborough Centre'
+  },
+  {
+    value: '22',
+    label: '22 – Scarborough-Agincourt'
+  },
+  {
+    value: '23',
+    label: '23 – Scarborough North'
+  },
+  {
+    value: '24',
+    label: '24 – Scarborough-Guildwood'
+  },
+  {
+    value: '25',
+    label: '25 – Scarborough-Rouge Park'
+  }
 ];
 
 export const PROGRAM_OPTS = [
-    {
-        value: "Partnership Program",
-        label: "Partnership Program",
-        color: '#245C95'
-    },
-    {
-        value: "Outside the Box",
-        label: "Outside the Box",
-        color: '#B72941'
-    },
-    {
-        value: "StART Support",
-        label: "StART Support",
-        color: '#007F2E'
-    },
-    {
-        value: "Other",
-        label: "Other",
-        color: '#CFB51D'
-    }
+  {
+    value: "Partnership Program",
+    label: "Partnership Program",
+    color: '#245C95'
+  },
+  {
+    value: "Outside the Box",
+    label: "Outside the Box",
+    color: '#B72941'
+  },
+  {
+    value: "StART Support",
+    label: "StART Support",
+    color: '#007F2E'
+  },
+  {
+    value: "Other",
+    label: "Other",
+    color: '#CFB51D'
+  }
 ];
 
 export const SORT_OPTS = [
-    {
-        value: "1",
-        label: "Artist: A-Z"
-    },
-    {
-        value: "2",
-        label: "Artist: Z-A"
-    },
-    {
-        value: "3",
-        label: "Year: Low to High "
-    },
-    {
-        value: "4",
-        label: "Year: High to Low"
-    }
+  {
+    value: "1",
+    label: "Artist: A-Z"
+  },
+  {
+    value: "2",
+    label: "Artist: Z-A"
+  },
+  {
+    value: "3",
+    label: "Year: Low to High "
+  },
+  {
+    value: "4",
+    label: "Year: High to Low"
+  }
 ];
 
 export const ICONS_REG = {
-    "Partnership Program": {
-        icon: '/src/img/marker-blue.svg'
-    },
-    "Outside the Box": {
-        icon: '/src/img/marker-red.svg'
-    },
-    "StART Support": {
-        icon: '/src/img/marker-green.svg'
-    },
-    "Other": {
-        icon: '/src/img/marker-yellow.svg'
-    }
+  "Partnership Program": {
+    icon: '/src/img/marker-blue.svg'
+  },
+  "Outside the Box": {
+    icon: '/src/img/marker-red.svg'
+  },
+  "StART Support": {
+    icon: '/src/img/marker-green.svg'
+  },
+  "Other": {
+    icon: '/src/img/marker-yellow.svg'
+  }
 };
 
 export const ICONS_LRG = {
-    "Partnership Program": {
-        icon: '/src/img/marker-blue-l.svg'
-    },
-    "Outside the Box": {
-        icon: '/src/img/marker-red-l.svg'
-    },
-    "StART Support": {
-        icon: '/src/img/marker-green-l.svg'
-    },
-    "Other": {
-        icon: '/src/img/marker-yellow-l.svg'
-    }
+  "Partnership Program": {
+    icon: '/src/img/marker-blue-l.svg'
+  },
+  "Outside the Box": {
+    icon: '/src/img/marker-red-l.svg'
+  },
+  "StART Support": {
+    icon: '/src/img/marker-green-l.svg'
+  },
+  "Other": {
+    icon: '/src/img/marker-yellow-l.svg'
+  }
 };
--- src/sa.js
+++ src/sa.js
@@ -10,34 +10,34 @@ const SelectAll = props => {
     if (props.value  && props.value.length === props.options.length) {
       return (
         <ReactSelect
-          {...props}
-          value={[props.allOption]}
-          onChange={selected => {
-            let sel = selected.slice(1)
-            if (selected.length === 0){
-                return props.onChange(props.options) 
-            } else {
-              return props.onChange(selected.slice(1))
-            }
-          }}
-           
+        {...props}
+        value={[props.allOption]}
+        onChange={selected => {
+          let sel = selected.slice(1)
+          if (selected.length === 0){
+            return props.onChange(props.options)
+          } else {
+            return props.onChange(selected.slice(1))
+          }
+        }}
+
         />
       );
     }
     else if (props.value && props.value[0].label === "Select All") {
       return (
         <ReactSelect
-          {...props}
-          options={[props.allOption, ...props.options]}
-          value={[props.allOption]}
-          onChange={selected => props.onChange(selected.slice(1))}
+        {...props}
+        options={[props.allOption, ...props.options]}
+        value={[props.allOption]}
+        onChange={selected => props.onChange(selected.slice(1))}
         />
       );
-    } 
+    }
     else {
 
-    return (
-      <ReactSelect
+      return (
+        <ReactSelect
         {...props}
         options={[props.allOption, ...props.options]}
         onChange={selected => {
@@ -46,19 +46,19 @@ const SelectAll = props => {
             selected[selected.length - 1].value === props.allOption.value
           ) {
             return props.onChange(props.options);
-        } else if (selected.length === 0) {
+          } else if (selected.length === 0) {
             return props.onChange(props.options);
-        } else {
-          return props.onChange(selected);
-        }
-        
+          } else {
+            return props.onChange(selected);
+          }
+
         }}
-        
-      />
-   
-    );
-      }
-      }
+
+        />
+
+      );
+    }
+  }
 };
 
 
