aa951ee1d92e0910e37314cc3d9f21abab650535
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-03-08 15:11:10 -0400

    Added ability to keep non-image at front and still find cover image.

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,12 @@ export function handleMissingImage(e) {
   e.target.onerror = null;
   e.target.src = placeholder;
 }
+
+export const getCoverImage = (media) => {
+  for (const mediaItem of media) {
+    if (!!mediaItem.thumbnails) {
+      return mediaItem.thumbnails.large.url
+    }
+  }
+  return ''
+}
